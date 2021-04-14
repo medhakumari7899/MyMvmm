5d13c91925c90fb0fa9cfdf7d239d146a4c34d9d
medha <kumarimedha.itsmiths@gmail.com>
2021-04-14 18:24:34 +0530

    commited by medha

--- app/src/main/java/com/example/mymvm/SearchCountry.java
+++ app/src/main/java/com/example/mymvm/SearchCountry.java
@@ -2,6 +2,7 @@ package com.example.mymvm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,15 +13,23 @@ import com.hbb20.CountryCodePicker;
 
 public class SearchCountry extends AppCompatActivity {
      private CountryCodePicker ccp;
+     TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_country);
-        init();
-    }
-    private void init(){
-        ccp=findViewById(R.id.ccp);
+        textView=findViewById(R.id.textQr);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SearchCountry.this,ScanBarCodeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
